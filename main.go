9e9226f07a619297d0490c56af6ef6ed8556aad5
Tahsin Rahman <tr174@duke.edu>
2025-01-18 11:34:36 -0500

    worked on building url function

--- main.go
+++ main.go
@@ -12,7 +12,8 @@ import (
 
 
 var googleDomains = map[string]string{
-
+"com":"https://www.google.com/search?q=",
+"za": "https://www.google.co.za/search?q=",
 }
 
 type SearchResult struct{
@@ -31,20 +32,34 @@ func randomUserAgent() string{
 	return userAgents[randNum]
 }
 
-func buildGoogleUrls()(){
+func buildGoogleUrls(searchTerm, countryCode, pages, count int)([]string, error){
 	toScrape := []string{}
+	searchTerm = strings.Trim(searchTerm, " ")
+	searchTerm = strings.Replace(searchTerm, " ", "+", -1)
+	if googleBase, found := googleDomains[countryCode]; found{
+		for i := 0, i<pages ; i++{
+			start := i*count
+			scrapeURL := fmt.Sprints("%s%s&num=%d&hl=%s&start=%d&filter=0", googleBase, searchTerm, count, languageCode, start)
+		}
+	}
+	else{
+		err := fmt.Errorf("country (%s) is not currently supported", countryCode)
+		return nil, err
+	}
+	return toScrape, nil
 }
 
 
 
-func GoogleScrape()([]SearchResult, err){
+func GoogleScrape(searchTerm, countryCode, languageCodestring, pages, count)([]SearchResult, err){
 	results := []SearchResult{}
 	resultCounter := 0
-	buildGoogleUrls()
+	googlePages, err := buildGoogleUrls(searchTerm, countryCode, languageCode, pages, count)
+	
 }
 
 func main(){
-	res, err := GoogleScrape("Ethereum")
+	res, err := GoogleScrape("Ethereum", "en", "com", 1, 30)
 	if err == nil{
 		for _, res := range res{
 			fmt.Println(res)
