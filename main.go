9a865eb5b83c99dc14cc37a8dd5bfdfd8e490855
Tahsin Rahman <tr174@duke.edu>
2025-01-18 11:19:04 -0500

    Defined initial functions

--- main.go
+++ main.go
@@ -30,3 +30,25 @@ func randomUserAgent() string{
 	randNum := rand.Int() % len(userAgents)
 	return userAgents[randNum]
 }
+
+func buildGoogleUrls()(){
+	toScrape := []string{}
+}
+
+
+
+func GoogleScrape()([]SearchResult, err){
+	results := []SearchResult{}
+	resultCounter := 0
+	buildGoogleUrls()
+}
+
+func main(){
+	res, err := GoogleScrape("Ethereum")
+	if err == nil{
+		for _, res := range res{
+			fmt.Println(res)
+		}
+	}
+}
+
